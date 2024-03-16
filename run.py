00ed4095776b1801b9388a3238fe436752e94b4a
Gianlooca Zimmatore <gianluca.zimmatore@gmail.com>
2024-03-16 14:12:11 +0000

    Calculate surplus data

--- run.py
+++ run.py
@@ -72,7 +72,13 @@ def calculate_surplus_data(sales_row):
     """
     stock = SHEET.worksheet('stock').get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
+    
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    
+    return surplus_data
 
 def main():
     """
@@ -82,7 +88,8 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
 
 print('Welcome to Love Sandwiches Data Automation') # This is the first message that we're going to see before the functions inside the main function are called
 main() #in Python, you can't call a function above where it's defined, otherwise it will throw an error.
