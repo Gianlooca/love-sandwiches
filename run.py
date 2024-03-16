5aa534c2158bceb0891a7e652773abfbd0ea97c4
Gianlooca Zimmatore <gianluca.zimmatore@gmail.com>
2024-03-16 16:00:49 +0000

    Add calculated surplus data to surplus worksheet

--- run.py
+++ run.py
@@ -63,6 +63,15 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print('Sales worksheet updated successfully.\n')
 
+def update_surplus_worksheet(data):
+    """
+    Update surplus worksheet, add new row with list data provided.
+    """
+    print('Updating surplus worksheet...\n')
+    surplus_worksheet = SHEET.worksheet('surplus')
+    surplus_worksheet.append_row(data)
+    print('Surplus worksheet updated successfully.\n')
+
 def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculate the surplus for each item type.
@@ -89,7 +98,7 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
-    print(new_surplus_data)
+    update_surplus_worksheet(new_surplus_data)
 
 print('Welcome to Love Sandwiches Data Automation') # This is the first message that we're going to see before the functions inside the main function are called
 main() #in Python, you can't call a function above where it's defined, otherwise it will throw an error.
